361b047f2a676302f6748329bc604dd8abae76a0
maldisco <filipe.sf04@gmail.com>
2022-06-19 11:24:23 -0300

    Minor changer

--- src/Game.cpp
+++ src/Game.cpp
@@ -17,34 +17,29 @@ Game& Game::GetInstance()
 
 Game::Game(std::string title, int width, int height)
 {
-    if (instance != nullptr)
-    {
-        std::cout << "Já tem uma instancia criada doidão";
-    }
-
     instance = this;
 
     /* Initialization of basic SDL functionalities. */
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0)
     {
-        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        SDL_Log("Cant initialize SDL: %s", SDL_GetError());
         exit(EXIT_FAILURE);
     }
 
     if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF) == 0)
     {
-        SDL_Log("Unable to initialize IMG: %s", SDL_GetError());
+        SDL_Log("Cant initialize IMG: %s", SDL_GetError());
         exit(EXIT_FAILURE);
     }
 
     if (Mix_Init(MIX_INIT_OGG) == 0)
     {
-        SDL_Log("Unable to initialize MIX: %s", SDL_GetError());
+        SDL_Log("Cant initialize MIX: %s", SDL_GetError());
     }
 
     if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024) != 0)
     {
-        SDL_Log("Unable to initialize OpenAudio: %s", SDL_GetError());
+        SDL_Log("Cant initialize OpenAudio: %s", SDL_GetError());
         exit(EXIT_FAILURE);
     }
 
@@ -52,13 +47,13 @@ Game::Game(std::string title, int width, int height)
 
     window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
     if(window == nullptr){
-        SDL_Log("Unable to initialize window: %s", SDL_GetError());
+        SDL_Log("Cant initialize window: %s", SDL_GetError());
         exit(EXIT_FAILURE);
     }
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if(renderer == nullptr){
-        SDL_Log("Unable to initialize renderer: %s", SDL_GetError());
+        SDL_Log("Cant initialize renderer: %s", SDL_GetError());
         exit(EXIT_FAILURE);
     }
 
